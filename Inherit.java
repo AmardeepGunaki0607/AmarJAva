fe54fc3def46546c41fd1e3f6e3f1a02ffbd1b14
AmardeepGunaki0607 <amardeepgunaki@gmail.com>
2025-01-26 12:41:19 +0530

    Java prog1 26 Jan

---
+++ Inherit.java
@@ -0,0 +1,22 @@
+class Animal{
+	Animal(){
+	  System.out.println("Animal Constructor");
+}
+}
+
+class Mammal extends Animal{
+	Mammal(){
+	  System.out.println("Mammal constructor");
+}
+}
+
+class Dog extends Mammal{
+	Dog(){
+	  System.lout.prinln("Dog construstor");
+}
+}
+public class Main{
+	public static void main(Strind[] args){
+	  Dog myDog = new Dog();
+}
+}
